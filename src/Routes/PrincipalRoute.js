e756a5422642a74a1a45247b06f23bb0f06a455b
krnkenshi90 <krnkenshi90@gmail.com>
2025-02-10 18:08:14 -0500

    Save

--- src/Routes/PrincipalRoute.js
+++ src/Routes/PrincipalRoute.js
@@ -1,14 +1,26 @@
-import { getAllC } from "../Controller/PrincipalController.js";
-import { getAllP } from "../Controller/PrincipalController.js";
-import { getAllPe } from "../Controller/PrincipalController.js";
+import { getAllC, createCategoria, updateCategoria, deleteCategoria } from "../Controller/PrincipalController.js";
+import { getAllP, createProducto, updateProducto, deleteProducto } from "../Controller/PrincipalController.js";
+import { getAllPe, createPedido, updatePedido, deletePedido } from "../Controller/PrincipalController.js";
 import { getAllU } from "../Controller/PrincipalController.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get('/listarc', getAllC);
+router.post('/categoria', createCategoria);
+router.put('/categoria/:id', updateCategoria);
+router.delete('/categoria/:id', deleteCategoria);
+
 router.get('/listarp', getAllP);
-router.get('/listarpe', getAllPe);
-router.get('/listaru', getAllU)
+router.post('/producto', createProducto);
+router.put('/producto/:id', updateProducto);
+router.delete('/producto/:id', deleteProducto);
+
+router.get('/listarpe', getAllPe);                 
+router.post('/pedido', createPedido);
+router.put('/pedido/:id', updatePedido);
+router.delete('/pedido/:id', deletePedido);
+
+router.get('/listaru', getAllU);
 
 export default router;
